52e45b575e049dff57633d5e9aed221e402ac7e0
hyclew <hcw235@gmail.com>
2021-08-11 17:34:18 +0800

    调整ConfigUtil

--- DevopsTomcat/src/main/java/com/abc/util/ConfigUtil.java
+++ DevopsTomcat/src/main/java/com/abc/util/ConfigUtil.java
@@ -12,14 +12,21 @@ public class ConfigUtil {
 
     private static final String activeProfile;
     private static final String serverPort;
-    @Autowired
-    private static Environment env;
 
     static {
         Environment environment = SpringContextHolder.getApplicationContext().getEnvironment();
         System.out.println("static block:" + environment.getProperty("spring.profiles.active"));
         activeProfile = environment.getProperty("spring.profiles.active");
         serverPort = environment.getProperty("server.port");
+    }
+
+    private Environment env;
+
+    public static String read() {
+//        System.out.println(env.getProperty("spring.profiles.active"));
+        System.out.println("serverPort:" + serverPort);
+        System.out.println("activeProfile:" + activeProfile);
+        return activeProfile;
     }
 
 //    @Value("${server.port}")
@@ -36,11 +43,9 @@ public class ConfigUtil {
 //        System.out.println(env.getProperty("spring.profiles.active"));
 //    }
 
-    public static String read() {
-//        System.out.println(env.getProperty("spring.profiles.active"));
-        System.out.println("serverPort:" + serverPort);
-        System.out.println("activeProfile:" + activeProfile);
-        return activeProfile;
+    @Autowired
+    public void setEnv(Environment env) {
+        this.env = env;
     }
 
 
